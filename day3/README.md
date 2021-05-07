de937bf280cb6a9656dc336d0d4ba9239248cfbc
happylay <1803455421@qq.com>
2021-05-08 01:19:51 +0800

    🌜 脚手架搭建

---
+++ day3/README.md
@@ -0,0 +1,5 @@
+```
+npx create-react-app react-app          
+
+npm install react-router-dom
+```
---
+++ day3/react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './views/home'
+import Login from './views/login'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
